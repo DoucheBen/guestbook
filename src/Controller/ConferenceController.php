d8b131f468a18d0213ced869c6245c3d0211bde3
benoit <benoit.douche@gmail.com>
2023-05-15 21:00:56 +0200

    ui conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
